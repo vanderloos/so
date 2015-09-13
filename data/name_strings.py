090449b78046b5105330129052f699030cb5d28e
Oleksii Burdin <xolbu@berlingskemedia.dk>
2015-09-13 15:52:25 +0300

    Different strings

--- data/name_strings.py
+++ data/name_strings.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 names = [
-    #u"John O'Grady",
-    #u"John James \"Jimmy\" O'Grady",
+    u"John O\'Grady",
+    u"John James \"Jimmy\" O'Grady",
     u"Наина Иосифовна Ёлкина",
     u"Nguyễn Tấn Dũng",
     "NULL",
